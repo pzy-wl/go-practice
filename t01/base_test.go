b4060ae0464925fe3e5d8ef916d33c4907a7ac09
whr <whr@whr.local>
2020-06-30 14:00:45 +0800

    aaaa

---
+++ t01/base_test.go
@@ -0,0 +1,18 @@
+package t01
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_int(t *testing.T) {
+  a := 0
+  b := int64(1)
+  c := int8(1)
+  d := int16(1)
+  e := int32(40)
+  fmt.Println("---",b,"---",a,c,d,e)
+}
+
+
+

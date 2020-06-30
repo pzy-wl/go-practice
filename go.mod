a6afcda93104e1adc15e4797b13bc2bb245f7820
whr <whr@whr.local>
2020-06-30 18:19:03 +0800

    aaaa

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vhaoran/go-practice
 
 go 1.14
+
+require github.com/vhaoran/vchat v1.9.9
--- t01/base_test.go
+++ t01/base_test.go
@@ -3,6 +3,10 @@ package t01
 import (
 	"fmt"
 	"testing"
+	"time"
+
+	"github.com/vhaoran/vchat/common/g"
+	"github.com/vhaoran/vchat/lib/ylog"
 )
 
 // go test -run=Test_int
@@ -13,4 +17,31 @@ func Test_int(t *testing.T) {
 	d := int16(1)
 	e := int32(40)
 	fmt.Println("---", b, "---", a, c, d, e)
+}
+
+func Test_string(t *testing.T) {
+	fmt.Println("-----------------")
+	go func() {
+		fmt.Println("hello")
+	}()
+	go func() {
+		fmt.Println("hello 2")
+	}()
+	go func() {
+		fmt.Println("hello  3")
+	}()
+
+	time.Sleep(time.Second * 3)
+}
+
+func Test_ykit(t *testing.T) {
+	b := g.InSlice(1, []int{1, 2, 3, 4})
+	fmt.Println("-----------------")
+	fmt.Println(b)
+	ylog.Debug()
+
+	//ylog.Debug("--------base_test.go--->--b=", b)
+	//
+	//ylog.DebugDump("--------base_test.go------")
+	//ylog.Debug("aaaa")
 }

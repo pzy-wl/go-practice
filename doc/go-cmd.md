8b7f4f3ac35ca19a02eba739090863a8642b0749
whr <whr@whr.local>
2020-06-30 14:19:41 +0800

    aaaa

---
+++ doc/go-cmd.md
@@ -0,0 +1,10 @@
+go run main.go
+go build
+go vet 
+go version
+go env
+go get ./...
+go test Test_int
+
+go mod init <module_name>
+
--- t01/base_test.go
+++ t01/base_test.go
@@ -5,14 +5,12 @@ import (
 	"testing"
 )
 
+// go test -run=Test_int
 func Test_int(t *testing.T) {
-  a := 0
-  b := int64(1)
-  c := int8(1)
-  d := int16(1)
-  e := int32(40)
-  fmt.Println("---",b,"---",a,c,d,e)
+	a := 0
+	b := int64(1)
+	c := int8(1)
+	d := int16(1)
+	e := int32(40)
+	fmt.Println("---", b, "---", a, c, d, e)
 }
-
-
-
